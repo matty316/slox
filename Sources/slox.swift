f34429ff365ff2807e1f167fdc7aed756f1ab187
matty <mattreed1890@gmail.com>
2023-10-23 11:14:30 -0500

    add args parser

---
+++ Sources/slox.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import ArgumentParser
+
+@main
+struct slox: ParsableCommand {
+    mutating func run() throws {
+        print("hi")
+    }
+}
