7843210a2d9c6275612299c2529cdc54951baa89
matty <4nem@matty316.com>
2023-10-23 11:17:47 -0500

    fic dependency

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "slox"),
+            name: "slox",
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]),
         .testTarget(
             name: "sloxTests",
             dependencies: ["slox"]
