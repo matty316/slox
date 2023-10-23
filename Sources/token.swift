4e327cd13ce494d433e0f0cb325143eefa4e453f
matty <mattreed1890@gmail.com>
2023-10-23 10:51:40 -0500

    initial commit

---
+++ Sources/token.swift
@@ -0,0 +1,20 @@
+import Foundation 
+
+enum TokenType {
+    case LPAREN, RPAREN, LBRACE, RBRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    BANG, BANGEQ, EQ, EQEQ, GT, LT, GTEQ, LTEQ, 
+
+    IDENT, STRING, NUM,
+
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF 
+}
+
+struct Token {
+    let tokenType: TokenType
+    let lexeme: String
+    let literal: Any?
+    let line: UInt
+}
